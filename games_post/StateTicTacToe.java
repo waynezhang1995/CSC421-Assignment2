32245b50640e6eabdc1ae184f518f08690da483e
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-10-09 15:46:12 -0700

    Question 1 done

--- games_post/StateTicTacToe.java
+++ games_post/StateTicTacToe.java
@@ -18,34 +18,34 @@
  * of an extra element.
  *
  * In addition to the board array, the code stores a player
- * value to indicate whose turn it is.  
+ * value to indicate whose turn it is.
  */
 
 
 public class StateTicTacToe extends State {
-	
+
     public char board[] = new char [(3 * 3) + 1];
-    
+
     public StateTicTacToe() {
-    	
-        for (int i=0; i<10; i++) 
+
+        for (int i=0; i<10; i++)
             board[i] = ' ';
-        
+
         player = 1;
     }
-    
+
     public StateTicTacToe(StateTicTacToe state) {
-    	
+
         for(int i=0; i<10; i++)
-            this.board[i] = state.board[i]; 
-        
+            this.board[i] = state.board[i];
+
         player = state.player;
     }
-    
+
     public String toString() {
-    	
+
     	String ret = "";
-    	
+
     	for(int i=0; i<3; i++) {
     		for(int j=0; j<3; j++) {
     			ret += board[1 + i*3+j] + ", ";
